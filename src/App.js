fa91a598d29013c068b4f4bf4f0583dce0c96822
namwookyung <namwookyung.dev@gmail.com>
2022-09-13 18:01:14 +0900

    immer를 사용하여 더 쉽게 불변성 유지하기 학습 마무리_22.09.13

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useRef, useCallback, useState } from "react";
+import produce from "immer";
 
 const App = () => {
   const nextId = useRef(1);
@@ -9,16 +10,14 @@ const App = () => {
   });
 
   //input 수정을 위한 함수
-  const onChange = useCallback(
-    (e) => {
-      const { name, value } = e.target;
-      setForm({
-        ...form,
-        [name]: [value],
-      });
-    },
-    [form]
-  );
+  const onChange = useCallback((e) => {
+    const { name, value } = e.target;
+    setForm(
+      produce((draft) => {
+        draft[name] = value;
+      })
+    );
+  }, []);
 
   //form 등록을 위한 함수
   const onSubmit = useCallback(
@@ -31,10 +30,11 @@ const App = () => {
       };
 
       //array에 새 항목 등록
-      setData({
-        ...data,
-        array: data.array.concat(info),
-      });
+      setData(
+        produce((draft) => {
+          draft.array.push(info);
+        })
+      );
 
       //form 초기화
       setForm({
@@ -43,16 +43,20 @@ const App = () => {
       });
       nextId.current += 1;
     },
-    [data, form.name, form.username]
+    [form.name, form.username]
   );
 
   //항목을 삭제하는 함수
   const onRemove = useCallback(
     (id) => {
-      setData({
-        ...data,
-        array: data.array.filter((info) => info.id !== id),
-      });
+      setData(
+        produce((draft) => {
+          draft.array.splice(
+            draft.array.findIndex((info) => info.id === id),
+            1
+          );
+        })
+      );
     },
     [data]
   );
@@ -78,7 +82,7 @@ const App = () => {
         <ul>
           {data.array.map((info) => (
             <li key={info.id} onClick={() => onRemove(info.id)}>
-              {info.username}({info.name})
+              {info.username} ({info.name})
             </li>
           ))}
         </ul>
